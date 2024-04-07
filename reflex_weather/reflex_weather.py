ebc859f7ce345dbc8316bf5a09a1a28e804e7fdb
jjmurph33 <104844163+jjmurph33@users.noreply.github.com>
2024-04-07 15:46:49 -0400

    forecasts

--- reflex_weather/reflex_weather.py
+++ reflex_weather/reflex_weather.py
@@ -25,38 +25,55 @@ def zip_input() -> rx.Component:
                     placeholder=State.zipcode,
                     bg='white',
                 ),
-            rx.button('Lookup',style=style_button)
+            rx.button('Lookup',style=style_button),
         ),
         on_submit=State.lookup_button_handler,
         reset_on_submit=True,
     )
 
 
-def forecast_item(item: rx.Var[str]) -> rx.Component:
-    return rx.list_item(
-        rx.text(item),
+def forecast_item(data: dict) -> rx.Component:
+    name = data['name']
+    detail = data['detailedForecast']
+    temperature = data['temperature']
+    icon = data['icon']
+    return rx.card(
+        rx.vstack(
+            rx.text.strong(name,size='4'),
+            rx.text(detail,size='3'),
+            rx.hstack(
+                rx.image(src=icon),
+                rx.vstack(
+                    rx.text(f'{temperature} %',size='6'),
+                    align='center',
+                    justify='center'
+                )
+            ),
+            spacing='1'
+        )
     )
 
 
 def forecast_list() -> rx.Component:
-    return rx.container(
-        rx.unordered_list(
-            rx.foreach(State.forecast, forecast_item),
-                list_style_type='none',
-        )
+    return rx.vstack(
+        rx.foreach(State.forecast, forecast_item),
+        spacing='5'
     )
 
 def current_weather() -> rx.Component:
     return rx.vstack(
-        rx.text(f'Checking weather from {State.forecast_url}'),
+        rx.text(f'Checking weather from {State.forecast_url}',size='2'),
+        rx.spacer(),
         forecast_list(),
+        rx.spacer(),
         rx.accordion.root(
             rx.accordion.item(
                 header="raw data",
                 content=State.raw_data,
             ),
             collapsible=True,
-        )
+        ),
+        spacing='5',
     )
 
 
@@ -65,13 +82,13 @@ def index() -> rx.Component:
         rx.vstack(
             rx.heading('Weather Forecast'),
             zip_input(),
-            rx.cond(State.loaded,
-                    current_weather(),
-                    None),
             bg=rx.color('sky'),
-            padding="1em",
         ),
-        height="100vh",
+        rx.cond(State.loaded,
+                current_weather(),
+                None),
+        padding="1em",
+    height="100vh",
     )
 
 
